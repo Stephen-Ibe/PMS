f4c904d4d9b7d9106cdf4ff89d6edc373208b7c1
Stephen-Ibe <saintkalson@gmail.com>
2022-06-22 02:32:56 +0100

    connected MongoDB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
-const { graphqlHTTP } = require('express-graphql');
-
 const app = express();
+
+connectDB();
 
 app.use(
   '/graphql',
---
+++ server/model/client.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const ClientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    
+  }
+});
